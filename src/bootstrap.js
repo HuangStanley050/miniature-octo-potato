d9fdfc75cf583ebd61246affbfc3d8c9115919b4
Stanley Huang <infamous_godhand@yahoo.com>
2021-04-08 09:02:09 +1000

    refactor

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,14 @@
 import faker from "faker";
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+export const mount = (htmlElement) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.getElementById("dev-cart").innerHTML = cartText;
+  htmlElement.innerHTML = cartText;
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.getElementById("dev-cart");
+  if (el) {
+    mount(el);
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       name: "cart",
       filename: "remoteEntry.js",
       exposes: {
-        "./CartShow": "./src/index",
+        "./CartShow": "./src/bootstrap",
       },
       shared: {
         faker: { singleton: true },
